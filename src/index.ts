0966f3ff21f6ca5bd2a36f54d58a2de6002b4fd8
zus-pop <hoanganhtu3979@gmail.com>
2024-09-08 18:20:32 +0700

    first commit: setup back-end for project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (_: Request, res: Response) => {
+    res.send(`<h1>Hello world</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
