d2a44edb6a283d9295d0372e42e90da68361187b
HauPTD <haudtpse173226@fpt.edu.vn>
2024-09-08 23:07:25 +0700

    hauptf first commit

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (_: Request, res: Response) => {
-    res.send(`<h1>Hello world</h1>`);
-});
+// app.get("/", (_: Request, res: Response) => {
+//   res.send(`<h1>Hello world</h1>`);
+// });
+app.get("/data", (req, res) => {
+  //   res.json({ message: "Hello, world!" });
 
+  res.send("Hello, world!");
+});
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
