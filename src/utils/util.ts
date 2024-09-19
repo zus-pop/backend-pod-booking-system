767ae50495d84094ec48599d5af385e6589de226
zus-pop <hoanganhtu3979@gmail.com>
2024-09-19 15:26:46 +0700

    update generate slot function in order to insert into the database

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,10 @@
+import "dotenv/config";
 import moment from "moment";
 import { SlotOption } from "../types/type.ts";
 import { pool } from "../database/db.ts";
+import { ResultSetHeader } from "mysql2";
 
+const connection = await pool.getConnection();
 export const generateSlots = async (options: SlotOption) => {
     const formatType = "YYYY-MM-DD HH:mm:ss";
     const startDatetime = moment(options.startDate).set({
@@ -16,42 +19,47 @@ export const generateSlots = async (options: SlotOption) => {
         second: 0,
     });
 
-    while (startDatetime.isBefore(endDatetime)) {
-        const slot = {
-            pod_id: options.podId,
-            start_time: startDatetime.format(formatType),
-            end_time: startDatetime
-                .add(options.durationMinutes, "minutes")
-                .format(formatType),
-            unit_price: options.unitPrice,
-            is_available: true,
-        };
-        console.log(`From: ${slot.start_time} To: ${slot.end_time}`);
-        const sql = "INSERT INTO ?? SET ?";
-        const values = ["slot", slot];
-        console.log(pool.format(sql, values));
+    try {
+        while (startDatetime.isBefore(endDatetime)) {
+            const slot = {
+                pod_id: options.podId,
+                start_time: startDatetime.format(formatType),
+                end_time: startDatetime
+                    .add(options.durationMinutes, "minutes")
+                    .format(formatType),
+                unit_price: options.unitPrice,
+                is_available: true,
+            };
+            const sql = "INSERT INTO ?? SET ?";
+            const values = ["Slot", slot];
+            console.log(connection.format(sql, values));
+            await connection.query<ResultSetHeader>(sql, values);
 
-        if (options.gap) {
-            startDatetime.add(options.gap, "minutes");
-        }
+            if (options.gap) {
+                startDatetime.add(options.gap, "minutes");
+            }
 
-        if (startDatetime.hour() >= options.endHour) {
-            startDatetime.add(1, "day").set({
-                hour: options.startHour,
-                minute: 0,
-                second: 0,
-            });
-            console.log("=====================================");
+            if (startDatetime.hour() >= options.endHour) {
+                startDatetime.add(1, "day").set({
+                    hour: options.startHour,
+                    minute: 0,
+                    second: 0,
+                });
+            }
         }
+    } catch (err) {
+        console.error("Error in generating slot: ", err);
+    } finally {
+        connection.release();
     }
 };
 
 await generateSlots({
-    startDate: "2022-01-01",
-    endDate: "2022-01-01",
+    startDate: "2024-09-22",
+    endDate: "2024-09-22",
     startHour: 7,
-    endHour: 12,
+    endHour: 22,
     durationMinutes: 60,
-    podId: 30,
-    unitPrice: 329000,
+    podId: 1,
+    unitPrice: 85000,
 });
