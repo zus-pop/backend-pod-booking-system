cd73e4263db690a0dc86ec54ccf12c6697b0713c
zus-pop <hoanganhtu3979@gmail.com>
2024-09-20 02:15:32 +0700

    integrate database for POD api endpoint

--- src/controllers/PODController.ts
+++ src/controllers/PODController.ts
@@ -2,24 +2,23 @@ import { Request, Response } from "express";
 import PODService from "../services/PODService.ts";
 
 const findAll = async (req: Request, res: Response) => {
-    res.json([
-        {
-            pod_id: 1,
-            pod_name: 'POD single'
-        },
-        {
-            pod_id: 2,
-            pod_name: 'POD pair'
-        },
-    ]);
-}
+    const pod = await PODService.findAll();
+    if (!pod) {
+        return res.status(404).json({ message: "No POD found" });
+    }
+    return res.status(200).json(pod);
+};
 
 const findById = async (req: Request, res: Response) => {
     const { id } = req.params;
-    res.json({ message: id });
-}
+    const pod = await PODService.findById(+id);
+    if (!pod) {
+        return res.status(404).json({ message: "No POD found" });
+    }
+    return res.status(200).json(pod);
+};
 
 export default {
     findAll,
     findById,
-}
+};
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,3 +1,5 @@
+import { RowDataPacket } from "mysql2";
+
 export interface SlotOption {
     startDate: string;
     endDate: string;
@@ -7,4 +9,14 @@ export interface SlotOption {
     podId: number;
     unitPrice: number;
     gap?: number;
+}
+
+export interface POD extends RowDataPacket {
+    pod_id: number;
+    pod_name: string;
+    type_id: number;
+    description?: string;
+    image?: string;
+    is_available: boolean;
+    store_id?: number;
 }
