de588bfeb9d339ed79e07ee63e80ecd233c78e9e
zus-pop <hoanganhtu3979@gmail.com>
2024-09-25 23:58:07 +0700

    implemented controller and route layer for Slot resource

--- src/v1/routes/PODRoute.ts
+++ src/v1/routes/PODRoute.ts
@@ -1,7 +1,10 @@
-import { Router } from 'express'
-import PODController from '../../controllers/PODController.ts';
+import { Router } from "express";
+import PODController from "../../controllers/PODController.ts";
 
-export const PODRouter = Router()
+export const PODRouter = Router();
 
+// GET: api/v1/pods
 PODRouter.get("/", PODController.findAll);
+
+// GET: api/v1/pods/:id
 PODRouter.get("/:id", PODController.findById);
