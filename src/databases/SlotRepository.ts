ef080b833666b70931ae5036cec4c95ce4c03b53
zus-pop <hoanganhtu3979@gmail.com>
2024-09-25 16:46:02 +0700

    implement findById for SlotRepository

--- src/databases/SlotRepository.ts
+++ src/databases/SlotRepository.ts
@@ -17,8 +17,28 @@ const findAll = async () => {
             "is_available",
         ];
         const values = [colums, "Slot"];
-        const [rows] = await connection.query<Slot[]>(sql, values);
-        return rows;
+        const [slots] = await connection.query<Slot[]>(sql, values);
+        return slots;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+};
+
+const findById = async (id: number) => {
+    try {
+        const sql = "SELECT ?? FROM ?? WHERE ?? = ?";
+        const colums = [
+            "slot_id",
+            "pod_id",
+            "start_time",
+            "end_time",
+            "unit_price",
+            "is_available",
+        ];
+        const values = [colums, "Slot", "slot_id", id];
+        const [slots] = await connection.query<Slot[]>(sql, values);
+        return slots[0];
     } catch (err) {
         console.log(err);
         return null;
