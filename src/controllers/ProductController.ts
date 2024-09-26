8e3a5e54daf861cb8361616282ac87f422389e69
hauptd <haudtpse173226@fpt.edu.vn>
2024-09-26 09:59:57 +0700

    complete findById for product

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -9,6 +9,16 @@ const findAll = async (_: Request, res: Response) => {
   return res.status(200).json(product);
 };
 
+const findById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await ProductService.findProductById(+id);
+  if (!product) {
+    return res.status(404).json({ message: "No POD found" });
+  }
+  return res.status(200).json(product);
+};
+
 export default {
   findAll,
+  findById,
 };
--- src/v1/routes/ProductRoute.ts
+++ src/v1/routes/ProductRoute.ts
@@ -4,3 +4,4 @@ import ProductController from "../../controllers/ProductController.ts";
 export const ProductRouter = Router();
 
 ProductRouter.get("/", ProductController.findAll);
+ProductRouter.get("/:id", ProductController.findById);
