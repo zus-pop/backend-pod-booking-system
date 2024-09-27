37fe63154e786990b49310a791733a4bab39063e
hauptd <haudtpse173226@fpt.edu.vn>
2024-09-27 22:45:29 +0700

    complete create and update for product

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,26 +1,50 @@
 import ProductRepository from "../repositories/ProductRepository.ts";
+import { Product } from "../types/type.ts";
 
 const findAllProduct = async () => {
-    try {
-        const products = await ProductRepository.findAll();
-        return products;
-    } catch (err) {
-        console.log(err);
-        return null;
-    }
+  try {
+    const products = await ProductRepository.findAll();
+    return products;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 };
 
 const findProductById = async (id: number) => {
-    try {
-        const product = await ProductRepository.findById(id);
-        return product;
-    } catch (err) {
-        console.log(err);
-        return null;
-    }
+  try {
+    const product = await ProductRepository.findById(id);
+    return product;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const createNewProduct = async (productData: Product) => {
+  try {
+    const newProductId = await ProductRepository.createNewProduct(productData);
+    const newProduct = await ProductRepository.findById(newProductId);
+    return newProduct;
+  } catch (err) {
+    console.error("Error creating new product:", err);
+    throw new Error("Unable to create new product");
+  }
+};
+
+const updateProduct = async (product: Product) => {
+  try {
+    const updated = await ProductRepository.updateProduct(product);
+    return updated;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 export default {
-    findAllProduct,
-    findProductById,
+  findAllProduct,
+  findProductById,
+  createNewProduct,
+  updateProduct,
 };
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,99 +1,99 @@
 import { RowDataPacket } from "mysql2";
 export interface SlotOption {
-    startDate: string;
-    endDate: string;
-    startHour: number;
-    endHour: number;
-    durationMinutes: number;
-    podId: number;
-    unitPrice: number;
-    gap?: number;
+  startDate: string;
+  endDate: string;
+  startHour: number;
+  endHour: number;
+  durationMinutes: number;
+  podId: number;
+  unitPrice: number;
+  gap?: number;
 }
 export interface POD extends RowDataPacket {
-    pod_id: number;
-    pod_name: string;
-    type_id: number;
-    description?: string;
-    image?: string;
-    is_available: boolean;
-    store_id?: number;
+  pod_id: number;
+  pod_name: string;
+  type_id: number;
+  description?: string;
+  image?: string;
+  is_available: boolean;
+  store_id?: number;
 }
 export interface User extends RowDataPacket {
-    user_id?: number;
-    email: string;
-    password: string;
-    user_name: string;
-    avatar?: string;
-    role_id: number;
-    phone_number?: string;
-    created_at?: Date | string;
-    updated_at?: Date | string;
+  user_id?: number;
+  email: string;
+  password: string;
+  user_name: string;
+  avatar?: string;
+  role_id: number;
+  phone_number?: string;
+  created_at?: Date | string;
+  updated_at?: Date | string;
 }
 
 export interface Product extends RowDataPacket {
-    product_id?: number;
-    product_name?: string;
-    catedory_id?: number;
-    image?: string;
-    description?: string;
-    price?: number;
-    store_id?: number;
-    stock?: number;
+  product_id?: number;
+  product_name?: string;
+  catedory_id?: number;
+  image?: string;
+  description?: string;
+  price?: number;
+  store_id?: number;
+  stock?: number;
 }
 
 export interface BookingProduct extends RowDataPacket {
-    booking_id?: number;
-    product_id?: number;
-    unit_price?: number;
-    quantity?: number;
+  booking_id?: number;
+  product_id?: number;
+  unit_price?: number;
+  quantity?: number;
 }
 
 export interface Category extends RowDataPacket {
-    category_id: number;
-    category_name: string;
+  category_id: number;
+  category_name: string;
 }
 
 export interface Store extends RowDataPacket {
-    store_id: number;
-    store_name: string;
-    address: string;
-    hotline: string;
+  store_id: number;
+  store_name: string;
+  address: string;
+  hotline: string;
 }
 
 export interface Slot extends RowDataPacket {
-    slot_id: number;
-    pod_id: number;
-    start_time: Date;
-    end_time: Date;
-    unit_price: number;
-    is_available: boolean;
+  slot_id: number;
+  pod_id: number;
+  start_time: Date;
+  end_time: Date;
+  unit_price: number;
+  is_available: boolean;
 }
 
 export interface PODType extends RowDataPacket {
-    type_id: number;
-    type_name: string;
-    capacity: number;
+  type_id: number;
+  type_name: string;
+  capacity: number;
 }
 
 export interface Booking extends RowDataPacket {
-    booking_id?: number;
-    pod_id?: number;
-    slot_id?: number;
-    user_id?: number;
-    booking_date?: Date | string;
-    booking_status?: keyof typeof BookingStatus;
+  booking_id?: number;
+  pod_id?: number;
+  slot_id?: number;
+  user_id?: number;
+  booking_date?: Date | string;
+  booking_status?: keyof typeof BookingStatus;
 }
 
 export enum BookingStatus {
-    Pending = "Pending",
-    Confirmed = "Confirmed",
-    Cancelled = "Cancelled",
-    Completed = "Completed",
+  Pending = "Pending",
+  Confirmed = "Confirmed",
+  Cancelled = "Cancelled",
+  Completed = "Completed",
 }
 
 export enum Role {
-    Admin = 1,
-    Customer = 2,
-    Manager = 3,
-    Staff = 4,
+  Admin = 1,
+  Customer = 2,
+  Manager = 3,
+  Staff = 4,
 }
