afb090ef6a40c3118bd6b97061dc7998c53caeb7
hauptd <haudtpse173226@fpt.edu.vn>
2024-09-27 22:15:37 +0700

    complete createNewProduct

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -3,22 +3,57 @@ import { Product } from "../types/type.ts";
 
 const connection = await pool.getConnection();
 const findAll = async () => {
-    const sql = "SELECT ?? FROM ??";
-    const colum = ["product_id", "product_name", "price", "stock"];
-    const values = [colum, "Product"];
-    const [product] = await connection.query<Product[]>(sql, values);
-    return product;
+  const sql = "SELECT ?? FROM ??";
+  const colum = ["product_id", "product_name", "price", "stock"];
+  const values = [colum, "Product"];
+  const [product] = await connection.query<Product[]>(sql, values);
+  return product;
 };
 
 const findById = async (id: number) => {
-    const sql = "SELECT ?? FROM ?? WHERE ?? = ?";
-    const colum = ["product_id", "product_name", "price", "stock"];
-    const values = [colum, "Product", "product_id", id];
-    const [product] = await connection.query<Product[]>(sql, values);
-    return product[0];
+  const sql = "SELECT ?? FROM ?? WHERE ?? = ?";
+  const colum = ["product_id", "product_name", "price", "stock"];
+  const values = [colum, "Product", "product_id", id];
+  const [product] = await connection.query<Product[]>(sql, values);
+  return product[0];
+};
+
+const createNewProduct = async (product: Product) => {
+  const sql =
+    "INSERT INTO ?? (??, ??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+  const table = "Product";
+  const columns = [
+    "product_name",
+    "category_id",
+    "image",
+    "description",
+    "price",
+    "store_id",
+    "stock",
+  ];
+
+  const values = [
+    product.product_name,
+    product.category_id,
+    product.image,
+    product.description,
+    product.price,
+    product.store_id,
+    product.stock,
+  ];
+
+  const [result]: any = await connection.query(sql, [
+    table,
+    ...columns,
+    ...values,
+  ]);
+
+  return result.insertId;
 };
 
 export default {
-    findAll,
-    findById,
+  findAll,
+  findById,
+  createNewProduct,
 };
