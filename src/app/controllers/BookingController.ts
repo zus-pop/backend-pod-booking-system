84f1638aca914f145cb55f619bf6f10c3d01e4e5
zus-pop <hoanganhtu3979@gmail.com>
2024-11-08 16:06:34 +0700

    add cash for product order

--- src/app/controllers/BookingController.ts
+++ src/app/controllers/BookingController.ts
@@ -3,132 +3,131 @@ import { Request, Response } from "express";
 import { Booking, BookingSlot, BookingStatus } from "../types/type.ts";
 
 const find = async (req: Request, res: Response) => {
-  const { booking_status, booking_date, page, limit } = req.query;
-  const result = await BookingService.find(
-    {
-      booking_status: booking_status as keyof typeof BookingStatus,
-      booking_date: booking_date as string,
-    },
-    {
-      page: page ? +page : 1,
-      limit: limit ? +limit : 10,
-    },
-    {
-      user: true,
+    const { booking_status, booking_date, page, limit } = req.query;
+    const result = await BookingService.find(
+        {
+            booking_status: booking_status as keyof typeof BookingStatus,
+            booking_date: booking_date as string,
+        },
+        {
+            page: page ? +page : 1,
+            limit: limit ? +limit : 10,
+        },
+        {
+            user: true,
+        }
+    );
+    if (!result || !result.bookings || !result.bookings.length) {
+        return res.status(404).json({ message: "No bookings found" });
     }
-  );
-  if (!result || !result.bookings || !result.bookings.length) {
-    return res.status(404).json({ message: "No bookings found" });
-  }
-  return res.status(200).json(result);
+    return res.status(200).json(result);
 };
 
 const findById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const booking = await BookingService.findBookingById(+id, {
-    pod: true,
-    slot: true,
-    product: true,
-    user: true,
-    payment: true,
-  });
-  if (!booking) {
-    return res.status(404).json({ message: "Booking not found" });
-  }
-  return res.status(200).json(booking);
+    const { id } = req.params;
+    const booking = await BookingService.findBookingById(+id, {
+        pod: true,
+        slot: true,
+        product: true,
+        user: true,
+        payment: true,
+    });
+    if (!booking) {
+        return res.status(404).json({ message: "Booking not found" });
+    }
+    return res.status(200).json(booking);
 };
 
 const findByUserId = async (req: Request, res: Response) => {
-  const { payload } = req;
-  const { booking_status, booking_date, page, limit } = req.query;
-  const result = await BookingService.findByUserId(
-    payload.user_id,
-    {
-      booking_status: booking_status as keyof typeof BookingStatus,
-      booking_date: booking_date as string,
-    },
-    {
-      page: page ? +page : 1,
-      limit: limit ? +limit : 8,
-    },
-    {
-      user: true,
+    const { payload } = req;
+    const { booking_status, booking_date, page, limit } = req.query;
+    const result = await BookingService.findByUserId(
+        payload.user_id,
+        {
+            booking_status: booking_status as keyof typeof BookingStatus,
+            booking_date: booking_date as string,
+        },
+        {
+            page: page ? +page : 1,
+            limit: limit ? +limit : 8,
+        },
+        {
+            user: true,
+        }
+    );
+    if (!result || !result.bookings || !result.bookings.length) {
+        return res.status(404).json({ message: "No bookings found" });
     }
-  );
-  if (!result || !result.bookings || !result.bookings.length) {
-    return res.status(404).json({ message: "No bookings found" });
-  }
-  return res.status(200).json(result);
+    return res.status(200).json(result);
 };
 
 const create = async (req: Request, res: Response) => {
-  const {
-    booking,
-    bookingSlots,
-  }: { booking: Booking; bookingSlots: BookingSlot[] } = req.body;
-  const { payload } = req;
-  const result = await BookingService.createABooking(
-    booking,
-    bookingSlots,
-    payload.user_id
-  );
-  if (!result) {
-    return res.status(400).json({ message: "Failed to create booking" });
-  }
-  return res.status(200).json(result);
+    const {
+        booking,
+        bookingSlots,
+    }: { booking: Booking; bookingSlots: BookingSlot[] } = req.body;
+    const { payload } = req;
+    const result = await BookingService.createABooking(
+        booking,
+        bookingSlots,
+        payload.user_id
+    );
+    if (!result) {
+        return res.status(400).json({ message: "Failed to create booking" });
+    }
+    return res.status(200).json(result);
 };
 
 const update = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const booking: Booking = {
-    ...req.body,
-    booking_id: id,
-  };
-  const result = await BookingService.updateABooking(booking);
-  if (!result || !result.affectedRows) {
-    return res.status(404).json({ message: "Booking not found" });
-  }
-  return res.status(200).json({ result, message: "Update successfully" });
+    const { id } = req.params;
+    const booking: Booking = {
+        ...req.body,
+        booking_id: id,
+    };
+    const result = await BookingService.updateABooking(booking);
+    if (!result || !result.affectedRows) {
+        return res.status(404).json({ message: "Booking not found" });
+    }
+    return res.status(200).json({ result, message: "Update successfully" });
 };
 
 const getBookingsCountByPod = async (req: Request, res: Response) => {
-  try {
-    const bookingCounts = await BookingService.countBookingsByPod();
-    if (!bookingCounts || bookingCounts.length === 0) {
-      return res.status(404).json({ message: "No bookings found" });
+    try {
+        const bookingCounts = await BookingService.countBookingsByPod();
+        if (!bookingCounts || bookingCounts.length === 0) {
+            return res.status(404).json({ message: "No bookings found" });
+        }
+        return res.status(200).json(bookingCounts);
+    } catch (error) {
+        console.error("Error fetching booking counts by pod:", error);
+        return res.status(500).json({ message: "Internal server error" });
     }
-    return res.status(200).json(bookingCounts);
-  } catch (error) {
-    console.error("Error fetching booking counts by pod:", error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
 };
 
 export const getBookingsCountByPODType = async (
-  req: Request,
-  res: Response
+    req: Request,
+    res: Response
 ) => {
-  try {
-    const bookingCounts = await BookingService.countBookingsByPODType();
-    console.log(bookingCounts);
+    try {
+        const bookingCounts = await BookingService.countBookingsByPODType();
 
-    if (!bookingCounts || bookingCounts.length === 0) {
-      return res.status(404).json({ message: "No bookings found" });
+        if (!bookingCounts || bookingCounts.length === 0) {
+            return res.status(404).json({ message: "No bookings found" });
+        }
+
+        return res.status(200).json(bookingCounts);
+    } catch (error) {
+        console.error("Error fetching booking counts by POD type:", error);
+        return res.status(500).json({ message: "Internal server error" });
     }
-
-    return res.status(200).json(bookingCounts);
-  } catch (error) {
-    console.error("Error fetching booking counts by POD type:", error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
 };
 
 export default {
-  find,
-  findById,
-  findByUserId,
-  create,
-  update,
-  getBookingsCountByPod,
-  getBookingsCountByPODType,
+    find,
+    findById,
+    findByUserId,
+    create,
+    update,
+    getBookingsCountByPod,
+    getBookingsCountByPODType,
 };
--- src/app/services/BookingProductService.ts
+++ src/app/services/BookingProductService.ts
@@ -1,127 +1,172 @@
+import moment from "moment";
 import { pool } from "../config/pool.ts";
 import BookingProductRepository from "../repositories/BookingProductRepository.ts";
 import { BookingProduct } from "../types/type.ts";
 import { createOnlinePaymentRequest } from "../utils/zalo.ts";
+import PaymentRepository from "../repositories/PaymentRepository.ts";
+import ProductRepository from "../repositories/ProductRepository.ts";
 
 const findAllBookingProducts = async () => {
-  const connection = await pool.getConnection();
-  try {
-    const bookingProducts = await BookingProductRepository.findAll(connection);
-    return bookingProducts;
-  } catch (err) {
-    console.error(err);
-    return null;
-  } finally {
-    connection.release();
-  }
+    const connection = await pool.getConnection();
+    try {
+        const bookingProducts = await BookingProductRepository.findAll(
+            connection
+        );
+        return bookingProducts;
+    } catch (err) {
+        console.error(err);
+        return null;
+    } finally {
+        connection.release();
+    }
 };
 
 const findByBookingId = async (booking_id: number) => {
-  const connection = await pool.getConnection();
-  try {
-    const bookingProducts = await BookingProductRepository.findByBookingId(
-      booking_id,
-      connection
-    );
-    return bookingProducts;
-  } catch (err) {
-    console.error(err);
-    return null;
-  } finally {
-    connection.release();
-  }
+    const connection = await pool.getConnection();
+    try {
+        const bookingProducts = await BookingProductRepository.findByBookingId(
+            booking_id,
+            connection
+        );
+        return bookingProducts;
+    } catch (err) {
+        console.error(err);
+        return null;
+    } finally {
+        connection.release();
+    }
 };
 
 const findByBookingIdAndSlotId = async (
-  booking_id: number,
-  slot_id: number
+    booking_id: number,
+    slot_id: number
 ) => {
-  const connection = await pool.getConnection();
-  try {
-    const bookingProducts =
-      await BookingProductRepository.findByBookingIdAndSlotId(
-        booking_id,
-        slot_id,
-        connection
-      );
-    return bookingProducts;
-  } catch (err) {
-    console.error(err);
-    return null;
-  } finally {
-    connection.release();
-  }
+    const connection = await pool.getConnection();
+    try {
+        const bookingProducts =
+            await BookingProductRepository.findByBookingIdAndSlotId(
+                booking_id,
+                slot_id,
+                connection
+            );
+        return bookingProducts;
+    } catch (err) {
+        console.error(err);
+        return null;
+    } finally {
+        connection.release();
+    }
 };
 
 const findAllSlotByBookingIdAndPaymentId = async (
-  booking_id: number,
-  payment_id: number
+    booking_id: number,
+    payment_id: number
 ) => {
-  const connection = await pool.getConnection();
-  try {
-    const bookingProducts =
-      await BookingProductRepository.findAllSlotByBookingIdAndPaymentId(
-        booking_id,
-        payment_id,
-        connection
-      );
-    return bookingProducts;
-  } catch (err) {
-    console.error(err);
-    return null;
-  } finally {
-    connection.release();
-  }
+    const connection = await pool.getConnection();
+    try {
+        const bookingProducts =
+            await BookingProductRepository.findAllSlotByBookingIdAndPaymentId(
+                booking_id,
+                payment_id,
+                connection
+            );
+        return bookingProducts;
+    } catch (err) {
+        console.error(err);
+        return null;
+    } finally {
+        connection.release();
+    }
 };
 
 const createProductPayment = async (
-  bookingProduct: BookingProduct[],
-  user_id: number
+    bookingProduct: BookingProduct[],
+    user_id: number
 ) => {
-  const amount = bookingProduct.reduce(
-    (acc, cur) => acc + cur.unit_price! * cur.quantity!,
-    0
-  );
-  const { return_code, sub_return_message, order_url } =
-    await createOnlinePaymentRequest({
-      user_id,
-      amount,
-      expire_duration_seconds: 600,
-      item: bookingProduct,
-      callback_url: "callback-product",
-      redirect_url: "",
-    });
-  if (return_code !== 1) throw new Error(sub_return_message);
-  return {
-    payment_url: order_url,
-    message: sub_return_message,
-  };
+    const amount = bookingProduct.reduce(
+        (acc, cur) => acc + cur.unit_price! * cur.quantity!,
+        0
+    );
+    const { return_code, sub_return_message, order_url } =
+        await createOnlinePaymentRequest({
+            user_id,
+            amount,
+            expire_duration_seconds: 600,
+            item: bookingProduct,
+            callback_url: "callback-product",
+            redirect_url: "",
+        });
+    if (return_code !== 1) throw new Error(sub_return_message);
+    return {
+        payment_url: order_url,
+        message: sub_return_message,
+    };
 };
 
-const addProductForBooking = async (bookingProduct: BookingProduct[]) => {
-  const connection = await pool.getConnection();
-  try {
-    await connection.beginTransaction();
-    const result = await BookingProductRepository.create(
-      bookingProduct,
-      connection
-    );
-    await connection.commit();
-    return result.affectedRows;
-  } catch (err) {
-    console.log(err);
-    await connection.rollback();
-    return null;
-  } finally {
-    connection.release();
-  }
+const addProductForBooking = async (bookingProducts: BookingProduct[]) => {
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
+        const paymentResult = await PaymentRepository.create(
+            {
+                transaction_id: `${moment().format("YYMMDD")}_${Math.floor(
+                    Math.random() * 1000000
+                )}`,
+                booking_id: bookingProducts[0].booking_id,
+                total_cost: bookingProducts.reduce(
+                    (acc, cur) => acc + cur.unit_price! * cur.quantity!,
+                    0
+                ),
+                payment_date: moment()
+                    .utcOffset(+7)
+                    .format("YYYY-MM-DD HH:mm:ss"),
+                payment_status: "Paid",
+                payment_for: "Product",
+            },
+            connection
+        );
+
+        const productResult = await BookingProductRepository.create(
+            bookingProducts.map((bookingProduct) => ({
+                ...bookingProduct,
+                payment_id: paymentResult.insertId,
+            })),
+            connection
+        );
+
+        if (productResult.affectedRows) {
+            for (const bookingProduct of bookingProducts) {
+                const product = await ProductRepository.findById(
+                    bookingProduct.product_id!,
+                    connection
+                );
+                const newStock = product.stock! - bookingProduct.quantity!;
+                await ProductRepository.updateProduct(
+                    {
+                        product_id: product.product_id,
+                        stock: newStock,
+                    },
+                    connection
+                );
+            }
+        }
+
+        await connection.commit();
+        return {message: "Booking Product Added Successfully"};
+    } catch (err) {
+        console.log(err);
+        await connection.rollback();
+        return null;
+    } finally {
+        connection.release();
+    }
 };
 
 export default {
-  findAllBookingProducts,
-  findByBookingId,
-  findByBookingIdAndSlotId,
-  findAllSlotByBookingIdAndPaymentId,
-  addProductForBooking,
-  createProductPayment,
+    findAllBookingProducts,
+    findByBookingId,
+    findByBookingIdAndSlotId,
+    findAllSlotByBookingIdAndPaymentId,
+    addProductForBooking,
+    createProductPayment,
 };
